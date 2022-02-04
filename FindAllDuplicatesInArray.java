cbb25c6dfbd9c9a30516d788574f311ee5e09392
sudheerhadoop <sudhe9630@gmail.com>
2022-02-04 11:43:41 +0530

    Create FindAllDuplicatesInArray.java

---
+++ FindAllDuplicatesInArray.java
@@ -0,0 +1,17 @@
+
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+         ArrayList<Integer> al = new ArrayList();
+        for(int i=0;i<nums.length;i++)
+        {
+            int index = Math.abs(nums[i]) - 1;
+            if(nums[index] < 0)
+            {
+                al.add(index + 1);
+            }
+            nums[index] = -nums[index];
+        }
+        
+        return al;
+    }
+}
